d5f321f906dad63ffe35fc211d4de1e98ae43270
cloudhead <self@cloudhead.net>
2010-11-27 18:31:09 -0500

    (test) database-test fixes/additions

--- test/database-test.js
+++ test/database-test.js
@@ -36,7 +36,7 @@ vows.describe('resourcer/engines/database').addVows({
 }).addVows({
     "A default Resource factory" : {
         topic: function() {
-            return resourcer.defineResource(function () {
+            return this.Factory = resourcer.defineResource(function () {
                 this.use('database');
             });
         },
@@ -58,7 +58,7 @@ vows.describe('resourcer/engines/database').addVows({
                 }
             }
         },
-        /*"a get() request": {
+        "a get() request": {
             "when successful": {
                 topic: function (r) {
                     return r.get('bob', this.callback);
@@ -69,7 +69,8 @@ vows.describe('resourcer/engines/database').addVows({
                     assert.equal      (obj.constructor, resourcer.resources.Resource);
                 },
                 "should respond with the right object": function (e, obj) {
-                    assert.equal (obj._id, 'bob');
+                    assert.isNull (e);
+                    assert.equal  (obj._id, 'bob');
                 }
             },
             "when unsuccessful": {
@@ -77,44 +78,20 @@ vows.describe('resourcer/engines/database').addVows({
                     r.get("david", this.callback);
                 },
                 "should respond with an error": function (e, obj) {
-                    // Remark: Getting e.status === undefined instead of 404
+                    assert.equal       (e.error, 'not_found');
                     assert.equal       (e.status, 404);
                     assert.isUndefined (obj);
                 }
             }
-        },*/
-        /*"a find() request": {
-            // Remark: Database engine currently doesn't support 'find', should we add it?
-            "when successful": {
-                topic: function (r) {
-                    r.find({ hair: "black" }, this.callback);
-                },
-                "should respond with an array of length 2": function (e, obj) {
-                    assert.length (obj, 2);
-                },
-                "should respond with an array of Resource instances": function (e, obj) {
-                    assert.isArray    (obj);
-                    assert.instanceOf (obj[0], resourcer.resources.Resource);
-                    assert.instanceOf (obj[1], resourcer.resources.Resource);
-                }
-            },
-            "when unsuccessful": {
-                topic: function (r) { r.find({ hair: "blue" }, this.callback); },
-                "should respond with an empty array": function (e, obj) {
-                    assert.isArray (obj);
-                    assert.length  (obj, 0)
-                }
-            }
         },
         "an all() request": {
-            // Remark: Database engine currently doesn't support 'all', should we add it?
             topic: function (r) {
                 r.all(this.callback);
             },
             "should respond with an array of all records": function (e, obj) {
                 assert.isArray (obj);
-                assert.length  (obj, 3);
+                assert.length  (obj, 4);
             }
-        },*/
+        },
     }
 }).export(module);
